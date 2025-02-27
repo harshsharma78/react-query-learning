d7fc9464a64f75844994f8bff9ca1a1860bbf83a
Harsh Sharma <namastecoding@gmail.com>
2025-02-27 11:45:40 +0530

    Add routing with react-router-dom and restructure application layout

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,37 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./components/Layouts/MainLayout.jsx";
+import Home from "./Pages/Home.jsx";
+import FetchOld from "./Pages/FetchOld.jsx";
+import FetchRQ from "./Pages/FetchRQ.jsx";
 
-const queryClient = new QueryClient();
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/trad",
+        element: <FetchOld />,
+      },
+      {
+        path: "/rq",
+        element: <FetchRQ />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <RouterProvider router={router}>
+        <App />
+    </RouterProvider>
   </StrictMode>,
 );
