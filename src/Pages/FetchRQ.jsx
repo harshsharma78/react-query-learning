5823a5d13c2c95f37d14418c3a450e6c6119c6f9
Harsh Sharma <namastecoding@gmail.com>
2025-02-28 11:19:29 +0530

    Implement data fetching with React Query

--- src/Pages/FetchRQ.jsx
+++ src/Pages/FetchRQ.jsx
@@ -1,5 +1,39 @@
+import { fetchPosts } from "../api/api.js";
+import { useQuery } from "@tanstack/react-query";
+
 const FetchRQ = () => {
-  return <div>FetchRQ</div>;
+  const getPostsData = async () => {
+    try {
+      const res = await fetchPosts();
+      return res.status === 200 ? res.data : [];
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  };
+
+  const { isLoading, data, isError, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: getPostsData,
+  });
+
+  return (
+    <div>
+      <ul className="section-accordion">
+        {data?.map(curElem => {
+          const { id, title, body } = curElem;
+          return (
+            <li key={id}>
+              <p>{title}</p>
+              <p>{body}</p>
+            </li>
+          );
+        })}
+      </ul>
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>{error}</p>}
+    </div>
+  );
 };
 
 export default FetchRQ;
